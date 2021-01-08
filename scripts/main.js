12f9d89114e34a12aa54c5e32fbcabf566ecc424
QmelZ <59574967+QmelZ@users.noreply.github.com>
2021-01-08 16:00:21 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,34 @@ const graphite = new Effect(90, e => {
     Lines.line(e.x, e.y + 8, e.x, e.y - 8);
 });
 Blocks.graphitePress.craftEffect = graphite;
+
+// rgb force projector (code to steal)
+const disco = extend(Block, "disco-projector", {
+    localizedName: "Disco Projector",
+    description: "It's also square.",
+    buildVisibility: BuildVisibility.shown,
+    size: 3,
+    destructible: true,
+    update: true,
+    category: Category.effect,
+    alwaysUnlocked: true,
+    icons(){
+        return [Core.atlas.find(this.region)];
+    },
+    drawPlace(x, y, rot, val){
+        Draw.color(Color.red.cpy().shiftHue(Time.time));
+        Lines.stroke(1.2);
+        Lines.poly(x * Vars.tilesize, y * Vars.tilesize, 4, 216, 45);
+    }
+});
+
+disco.buildType = () => extend(Building, {
+    draw(){
+        Draw.z(Layer.block);
+        Draw.rect(Core.atlas.find(this.block.region), this.x, this.y)
+        Draw.z(Layer.shields);
+        Draw.color(Color.red.cpy().shiftHue(Time.time))
+        Fill.poly(this.x, this.y, 4, 216, 45);
+        Draw.reset();
+    }
+});
