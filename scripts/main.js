96b56eed4fc579a8eb5e9f80fa41eb2616cbd629
QmelZ <59574967+QmelZ@users.noreply.github.com>
2021-01-08 15:58:24 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 if(!Vars.headless){
     Core.app.post(() => {
-        Vars.mods.locateMod("rgb-graphite-press").meta.displayName = "[#ff0000]R[][#00ff00]G[][#0000ff]B[] [#b2c6d2]Graphite[] [lightgray]Press[]";
+        Vars.mods.locateMod("rgbdustry").meta.displayName = "[#ff0000]R[][#00ff00]G[][#0000ff]B[][accent]dustry[]";
     });
 };
 
-var h = ["000000", "ff0000", "00ff00", "0000ff", "00ffff", "ff00ff", "ffff00", "ffffff"];
-const effect1 = new Effect(90, e => {
-    Draw.color(Color.valueOf(h[Mathf.floorPositive(Time.time % 90 / 11.25)]));
+// rgb graphite press
+const graphite = new Effect(90, e => {
+    Draw.color(Color.red.cpy().shiftHue(Time.time));
     Lines.line(e.x + 8, e.y, e.x - 8, e.y);
     Lines.line(e.x, e.y + 8, e.x, e.y - 8);
 });
-Blocks.graphitePress.craftEffect = effect1;
+Blocks.graphitePress.craftEffect = graphite;
